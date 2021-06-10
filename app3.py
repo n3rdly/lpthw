4a013b0f8625c63eeea92b3d0fcaf2fcf0bfc17d
n3rdly <n3rdly@g33k.works>
2021-06-09 22:02:10 -0600

    module 3 and 4 done

---
+++ app3.py
@@ -0,0 +1,30 @@
+from math import * # imports the math module
+
+my_num = 5
+
+print(my_num) # Prints as a number
+print(str(my_num)) # Prints as a string
+print(str(my_num) + " my favorite number") # this lets you combine numbers with strings
+
+my_num = -5
+print(abs(my_num))
+print(pow(3, 2))  # 3 squared 
+print(pow(4, 6))  # 4 to the power of 6
+print(max(4, 6)) # prints max num
+print(min(4, 6)) # prints min num
+print(round(3.2)) # rounds down to 3
+print(round(3.7)) # rounds up to 4
+
+print(floor(3.7)) # Always rounds down
+print(ceil(3.7)) # Always rounds up
+print(sqrt(36)) # Square root of a number
+
+
+print(3 + 4.5) # Addition
+print(3 - 4.5) # Subtraction
+print(3 * 4.5) # Multiplication
+print(3 / 4.5) # Division
+print(3 * 4 + 5) # 3 times 4 then add 5
+print(3 * (4 + 5)) # Parentheses define order of operation
+print(10 % 3) # 10 divided by 3 and give the remainder (1 in this case)
+
